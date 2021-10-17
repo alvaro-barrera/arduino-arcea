6135d7b507fa49d369ad0a1b15076bc3ae3ad4a6
alvaro__barrera <alvaro_bv@hotmail.com>
2021-10-17 17:15:22 -0500

    Added directions for manual and voice commands

--- remotexy.ino
+++ remotexy.ino
@@ -1,72 +1,12 @@
-/*
-   -- New project --
-   
-   This source code of graphical user interface 
-   has been generated automatically by RemoteXY editor.
-   To compile this code using RemoteXY library 3.1.6 or later version 
-   download by link http://remotexy.com/en/library/
-   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/                   
-     - for ANDROID 4.8.01 or later version;
-     - for iOS 1.5.1 or later version;
-    
-   This source code is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.    
-*/
-
-//////////////////////////////////////////////
-//        RemoteXY include library          //
-//////////////////////////////////////////////
-
-// RemoteXY select connection mode and include library 
-#define REMOTEXY_MODE__HARDSERIAL
-
-#include <RemoteXY.h>
-
-// RemoteXY connection settings 
-#define REMOTEXY_SERIAL Serial
-#define REMOTEXY_SERIAL_SPEED 9600
-
-
-// RemoteXY configurate  
-#pragma pack(push, 1)
-uint8_t RemoteXY_CONF[] =
-  { 255,5,0,0,0,51,0,13,13,1,
-  4,128,14,15,33,5,2,26,1,0,
-  25,30,12,12,2,31,88,0,1,0,
-  6,47,12,12,2,31,88,0,1,0,
-  41,47,12,12,2,31,88,0,1,0,
-  24,65,12,12,2,31,88,0 };
-  
-// this structure defines all the variables and events of your control interface 
-struct {
-
-    // input variables
-  int8_t slider_1; // =0..100 slider position 
-  uint8_t button_1; // =1 if button pressed, else =0 
-  uint8_t button_2; // =1 if button pressed, else =0 
-  uint8_t button_3; // =1 if button pressed, else =0 
-  uint8_t button_4; // =1 if button pressed, else =0 
-
-    // other variable
-  uint8_t connect_flag;  // =1 if wire connected, else =0 
-
-} RemoteXY;
-#pragma pack(pop)
-
-/////////////////////////////////////////////
-//           END RemoteXY include          //
-/////////////////////////////////////////////
-
 #include <Servo.h>  
 
 Servo servo_main;
 
-const int PIN_BUTTON_1 = 13; //IN1 Derecha Atrás
-const int PIN_BUTTON_2 = 12; //IN2 Derecha 
-const int PIN_BUTTON_3 = 4; //IN3 Izquierda
-const int PIN_BUTTON_4 = 5; //IN4 Izquierda Atrás
+const int IN2 = 13; //IN1 Derecha Atrás
+const int IN1 = 12; //IN2 Derecha 
+
+const int IN3 = 4; //IN3 Izquierda
+const int IN4 = 5; //IN4 Izquierda Atrás
 
 
 const int servo_position_center = 90;
@@ -78,21 +18,46 @@ const int ultra_main_echo = 9;
 const int ultra_secondary_trig = 3;
 const int ultra_secondary_echo = 2;
 
+const String MANUAL_FORWARD = "manual_forward";
+const String MANUAL_REVERSE = "manual_reverse";
+const String MANUAL_RIGHT = "manual_right";
+const String MANUAL_LEFT = "manual_left";
+
+const String VOICE_FORWARD = "voice_forward";
+const String VOICE_REVERSE = "voice_rverse";
+const String VOICE_RIGHT = "voice_right";
+const String VOICE_LEFT = "voice_left";
+
+const String STOP = "stop";
+const String ROTATE = "rotate";
+
+const int speaker = 6;
+int frequency = 220;    // frequency correspondiente a la nota La
+int counter;          // variable para el counter
+const float m = 1.059; 
+
+ 
+/*
+
+input_remote=='1' => forward
+input_remote=='2' => reverse
+input_remote=='3' => left
+input_remote=='4' => right
+*/
+
+
+char input_remote;
+char last_input_remote = "";
+
 void setup() 
 {
-  RemoteXY_Init (); 
-
+  Serial.begin(9600);  
   servo_main.attach(10); 
-  RemoteXY.slider_1 = 50;
-  
-  pinMode (PIN_BUTTON_1, OUTPUT);
-  pinMode (PIN_BUTTON_2, OUTPUT);
-  pinMode (PIN_BUTTON_3, OUTPUT);
-  pinMode (PIN_BUTTON_4, OUTPUT);
-
-  
-  // TODO you setup code
-  
+    MANUAL_LEFT
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
 }
 
 void loop() 
@@ -104,76 +69,85 @@ void loop()
   int detection_distance = 20;
   int left_distance;
   int right_distance;
+
   servo_main.write(servo_position_center);
-  RemoteXY_Handler ();
 
-  /*int ms = (2500 - RemoteXY.slider_1 * 20); 
-  servo_main.writeMicroseconds(ms);*/
+  // conexion a bluetooth
+  if(Serial.available()>0){
+    input_remote = Serial.read();
+  }
 
-  if(RemoteXY.button_1==1) {
+  if (last_input_remote != "" && last_input_remote.find("voice") != std::string::npos && input_remote == "empty") {
+    input_remote = last_input_remote;
+  }
+
+  last_input_remote = last_input_remote;
+
+  Serial.println("input_remote: "+ String(input_remote));
+
+  // Forward
+  if(input_remote == MANUAL_FORWARD) {
     ultra_main = ultra(ultra_main_trig,ultra_main_echo);
-    if(ultra_main > detection_distance) {
-        goForward();
+    
+    if(ultra_main < detection_distance) {
+        stopForward();
+        alarm();
     }else { 
+        goForward();
         //Alarm
     }
-    /*digitalWrite(PIN_BUTTON_2, HIGH); //Derecha
-    digitalWrite(PIN_BUTTON_3, HIGH); //Izquierda*/
+    /*digitalWrite(IN1, HIGH); //Derecha
+    digitalWrite(IN3, HIGH); //Izquierda*/
   } else {
-    stopForward();
-    /*digitalWrite(PIN_BUTTON_2, LOW); //Derecha
-    digitalWrite(PIN_BUTTON_3, LOW); //Izquierda*/
+    // stopForward();
+    /*digitalWrite(IN1, LOW); //Derecha
+    digitalWrite(IN3, LOW); //Izquierda*/
   }
 
-  if(RemoteXY.button_2==1) {
+  if(input_remote == MANUAL_RIGHT) {
     right_distance = getDistance(servo_position_right, ultra_main_trig, ultra_main_echo);
-    if (right_distance > detection_distance) {
+    if(right_distance < detection_distance) {         
+        //  alarm();        
+         stopRight();
+    }else { 
+        // alarm();        
         goRight();
-    }else {
         //Alarm
     }
-    //digitalWrite(PIN_BUTTON_2, HIGH); //Derecha
   } else {
-    //digitalWrite(PIN_BUTTON_2, LOW); Not
+    //digitalWrite(IN1, LOW); Not
   }
 
-  if(RemoteXY.button_3==1) {
-    left_distance = getDistance(servo_position_left, ultra_main_trig, ultra_main_echo);
-    if (left_distance > detection_distance) {
-        goLeft(); 
+  if(input_remote == MANUAL_LEFT) {
+    left_distance = getDistance(servo_position_left, ultra_main_trig, ultra_main_echo);    
+    if (left_distance < detection_distance) {
+      stopLeft(); 
     }else {
-        //Alarm
+      goLeft();
     }
-    //digitalWrite(PIN_BUTTON_3, HIGH); //Izquierda
+    //digitalWrite(IN3, HIGH); //Izquierda
   } else {
-    //digitalWrite(PIN_BUTTON_3, LOW); Not
+    //digitalWrite(IN3, LOW); Not
   }
 
-  if(RemoteXY.button_4==1) {
+  if(input_remote == MANUAL_REVERSE) {
     ultra_secondary = ultra(ultra_secondary_trig,ultra_secondary_echo);
     if (ultra_secondary > detection_distance) {
         goReverse();
     }else {
         //Alarm
     }
-    /*digitalWrite(PIN_BUTTON_1, HIGH); //Derecha atrás
-    digitalWrite(PIN_BUTTON_4, HIGH); //Izquierda atrás*/
+    /*digitalWrite(IN2, HIGH); //Derecha atrás
+    digitalWrite(IN4, HIGH); //Izquierda atrás*/
   } else {
-    stopReverse();
-    /*digitalWrite(PIN_BUTTON_1, LOW); //Derecha
-    digitalWrite(PIN_BUTTON_4, LOW); //Izquierda*/
+    // stopReverse();
+    /*digitalWrite(IN2, LOW); //Derecha
+    digitalWrite(IN4, LOW); //Izquierda*/
   }
-  
-    
-  // TODO you loop code
-  // use the RemoteXY structure for data transfer
-  // do not call delay() 
-
-
 }
 
 // Ultrasonic control function
-int ultra(int Trig, int Echo)
+long ultra(int Trig, int Echo)
 {
   long duration;
   long distance;
@@ -185,13 +159,13 @@ int ultra(int Trig, int Echo)
   digitalWrite(Trig,LOW);
   
   duration = pulseIn(Echo,HIGH);
-  distance = (duration/2)/29;
+  distance = duration / 58;
   
   return distance;
 }
 
 // Get distance in any position
-int getDistance(int servo_position, int ultra_trig, int ultra_echo) {
+long getDistance(int servo_position, int ultra_trig, int ultra_echo) {
   long distance;
   servo_main.write(servo_position);
   delay(2000);
@@ -201,31 +175,53 @@ int getDistance(int servo_position, int ultra_trig, int ultra_echo) {
   return distance;
 }
 
-
 void goForward() {
-    digitalWrite(PIN_BUTTON_2, HIGH); //Derecha
-    digitalWrite(PIN_BUTTON_3, HIGH); //Izquierda
+  digitalWrite(IN1, HIGH); //Derecha
+  digitalWrite(IN3, HIGH); //Izquierda
 }
 
 void stopForward() {
-    digitalWrite(PIN_BUTTON_2, LOW); //Derecha
-    digitalWrite(PIN_BUTTON_3, LOW); //Izquierda
+  digitalWrite(IN1, LOW); //Derecha
+  digitalWrite(IN3, LOW); //Izquierda
 }
 
 void goRight() {
-    digitalWrite(PIN_BUTTON_2, HIGH); //Derecha
+  digitalWrite(IN3, HIGH); //Derecha
+}
+void stopRight() {
+  digitalWrite(IN3, LOW); //Derecha
 }
 
 void goLeft() {
-    digitalWrite(PIN_BUTTON_3, HIGH); //Izquierda
+  digitalWrite(IN1, HIGH); //Izquierda
+}
+void stopLeft() {
+  digitalWrite(IN1, LOW); //Izquierda
 }
 
 void goReverse() {
-    digitalWrite(PIN_BUTTON_1, HIGH); //Derecha atrás
-    digitalWrite(PIN_BUTTON_4, HIGH); //Izquierda atrás
+  digitalWrite(IN2, HIGH); //Derecha atrás
+  digitalWrite(IN4, HIGH); //Izquierda atrás
 }
 
 void stopReverse() {
-    digitalWrite(PIN_BUTTON_1, LOW); //Derecha
-    digitalWrite(PIN_BUTTON_4, LOW); //Izquierda
+  digitalWrite(IN2, LOW); //Derecha
+  digitalWrite(IN4, LOW); //Izquierda
+}
+
+void stopMotor() {
+  digitalWrite(IN1, LOW); //Derecha
+  digitalWrite(IN3, LOW); //Izquierda
+  digitalWrite(IN2, LOW); //Derecha
+  digitalWrite(IN4, LOW); //Izquierda
+}
+
+void alarm(){
+  for(counter=0,frequency=220;counter < 2;counter++){
+    frequency=frequency*m;     // actualiza la frequency
+    tone(speaker,frequency); // emite el tono
+    delay(1500);                 // lo mantiene 1.5 segundos
+    noTone(speaker);          // para el tono
+    delay(500);                  // espera medio segundo
+  }
 }
